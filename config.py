b625861c30ad63cb87bc8b79cf850662f3936d16
DEVIL-X <72757203+TEAMOFDEVIL-X@users.noreply.github.com>
2021-08-01 17:29:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 from heroku3 import from_key
 from pyrogram import Client
 
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", None)
+API_ID = int(os.environ.get("API_ID", "6"))
+API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 ARQ_API_KEY = "XQYJAL-HTSZIK-YALWDS-TJPWMO-ARQ" 
 LANGUAGE = "en"
